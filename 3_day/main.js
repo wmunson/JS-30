a704d3e6c7a29f7cd3de1b0c9f64f03607d295a9
Wes <munsonwa@gmail.com>
2017-04-21 11:50:40 -0400

    day 3

--- 3_day/main.js
+++ 3_day/main.js
@@ -4,3 +4,15 @@ var space = document.querySelector('input[name=space');
 var blur = document.querySelector('input[name=blur');
 var color = document.querySelector('input[name=color');
 
+var inputs = document.querySelectorAll('.control input');
+
+const handleUpdate = function(){
+	const suffix = this.dataset.sizing || '';
+	console.log(suffix);
+	console.log(this.value);
+	document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
+}
+
+inputs.forEach(input => input.addEventListener('change',handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove',handleUpdate));
