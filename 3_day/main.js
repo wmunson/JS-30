6800a8acab45149e3c1515fa02c0737d88791552
Wes <munsonwa@gmail.com>
2017-04-20 23:37:31 -0400

    day 3 pt 1

---
+++ 3_day/main.js
@@ -0,0 +1,6 @@
+var sub = document.querySelector('.sub');
+var img = document.querySelector('.image');
+var space = document.querySelector('input[name=space');
+var blur = document.querySelector('input[name=blur');
+var color = document.querySelector('input[name=color');
+
